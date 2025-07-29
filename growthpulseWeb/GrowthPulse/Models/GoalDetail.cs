21361c1f7974ef4c03fed69533c51dec6b799d02
Hashinirangala <rangala.dias@ifs.com>
2025-07-30 00:25:15 +0530

    Added GoalDetail model

---
+++ growthpulseWeb/GrowthPulse/Models/GoalDetail.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace GrowthPulse.Models
+{
+    public class GoalDetail
+    {
+        [Key]
+        public int GoalDetailId { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string GoalOwnerEmail { get; set; } = "";
+        [Column(TypeName = "nvarchar(100)")]
+        public string GoalName { get; set; } = "";
+        [Column(TypeName = "nvarchar(5)")]
+        public string GoalSetDate { get; set; } = "";
+    }
+}
