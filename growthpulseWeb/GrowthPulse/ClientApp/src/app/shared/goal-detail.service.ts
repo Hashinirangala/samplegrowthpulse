773314678e4e6f4cd558fd271f6c8d4612634ef0
Hashinirangala <hashini.rangala.dias@gmail.com>
2025-08-05 23:57:01 +0530

    Modified goal details service

--- growthpulseWeb/GrowthPulse/ClientApp/src/app/shared/goal-detail.service.ts
+++ growthpulseWeb/GrowthPulse/ClientApp/src/app/shared/goal-detail.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoalDetailService {
 
-  constructor() { }
+  url: string =  '/GoalDetails';
+
+  constructor(private http: HttpClient) { }
+
+  refreshList() {
+
+    this.http.get(this.url)
+    .subscribe({
+      next: res => {
+        console.log(res);
+      },
+      error: err => { console.log(err); }
+    })
+  }
 }
