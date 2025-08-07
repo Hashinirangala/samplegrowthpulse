b8bd9986f6d1c0ad290c25d5d31fb79c1ced0c65
Hashinirangala <hashini.rangala.dias@gmail.com>
2025-08-07 23:57:23 +0530

    Added goal dtail form

--- growthpulseWeb/GrowthPulse/ClientApp/src/app/goal-details/goal-detail-form/goal-detail-form.component.ts
+++ growthpulseWeb/GrowthPulse/ClientApp/src/app/goal-details/goal-detail-form/goal-detail-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GoalDetailService } from 'src/app/shared/goal-detail.service';
 
 @Component({
   selector: 'app-goal-detail-form',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalDetailFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service : GoalDetailService) { }
 
   ngOnInit(): void {
   }
