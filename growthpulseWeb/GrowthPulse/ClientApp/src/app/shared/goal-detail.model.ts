1b4583b37c8e1b35031cf51dd4e583a20d005dab
Hashinirangala <hashini.rangala.dias@gmail.com>
2025-08-28 00:27:03 +0530

    Fixed date displaying bug

--- growthpulseWeb/GrowthPulse/ClientApp/src/app/shared/goal-detail.model.ts
+++ growthpulseWeb/GrowthPulse/ClientApp/src/app/shared/goal-detail.model.ts
@@ -2,5 +2,5 @@ export class GoalDetail {
     goalDetailId: number = 0;
     goalOwnerEmail: string = '';
     goalName: string = '';
-    goalSetDate: Date = new Date();
+    goalSetDate: string = '';
 }
